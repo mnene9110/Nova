04d3cd3f9fc092517c68f74387c827a19d878dd7
Munene <mnene9110@gmail.com>
2026-04-05 19:38:08 +0000

    Fully link to   nova-78709908-62354   // Import the functions you need f

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8171025535-ef298",
-  "appId": "1:909821173119:web:f5c5a66b39d18f861a0e4a",
-  "apiKey": "AIzaSyCX8xXFHD5feGFnQ3q1vk89JCiaA8fJVyo",
-  "authDomain": "studio-8171025535-ef298.firebaseapp.com",
-  "databaseURL": "https://studio-8171025535-ef298-default-rtdb.europe-west1.firebasedatabase.app",
-  "measurementId": "",
-  "messagingSenderId": "909821173119"
+  apiKey: "AIzaSyADB7-ItQNYcqSmkzOp9TegzEqRU8Omq_o",
+  authDomain: "nova-78709908-62354.firebaseapp.com",
+  projectId: "nova-78709908-62354",
+  storageBucket: "nova-78709908-62354.firebasestorage.app",
+  messagingSenderId: "133262592734",
+  appId: "1:133262592734:web:0c95813e5a77979fa2904f"
 };
